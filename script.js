6f62db1a9b99c4e67cfb6dae4e014036d67e3c5b
zaraamjad5797 <zaraamjadab@gmail.com>
2026-08-27 02:40:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -233,4 +233,19 @@ function filterProducts(category) {
             product.style.display = "none";
         }
     });
+}function revealOnScroll() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    reveals.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+        const revealPoint = 100;
+
+        if (elementTop < windowHeight - revealPoint) {
+            element.classList.add("active");
+        }
+    });
 }
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
